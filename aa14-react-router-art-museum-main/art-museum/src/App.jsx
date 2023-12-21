0adb981db773c1221859305c50b5599af5cdb44b
wremsen <wcremsen@gmail.com>
2023-12-21 14:21:39 -0500

    server up an runnin!

---
+++ aa14-react-router-art-museum-main/art-museum/src/App.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { 
+    path: "*",
+    element: <h2>Page Not Found</h2>
+  }
+]);
+
+function App() {
+  return <RouterProvider router={router}/>;
+}
+
+export default App;
