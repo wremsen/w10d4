b154a19f83d99fdcf622b968a18e65790764683e
Jacob Tabor <jtabor214@gmail.com>
2023-12-21 17:33:22 -0500

    Phase 3

--- aa14-react-router-art-museum-main/art-museum/src/App.jsx
+++ aa14-react-router-art-museum-main/art-museum/src/App.jsx
@@ -1,18 +1,41 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import harvardArt from './data/harvardArt';
 import GalleryNavigation from './components/GalleryNavigation';
 
-
 console.log(harvardArt)
 
+function Layout(){
+  return (
+    <div className="page-wrapper">
+      <GalleryNavigation galleries={harvardArt.records} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
 const router = createBrowserRouter([
-  { 
-    path: "*",
-    element: <h2>Page Not Found</h2>
-  },{
-    path: '/galleries',
-    element: <GalleryNavigation galleries={harvardArt.records} />
-  }
+{
+  element : <Layout/>,
+  children: [
+    {
+      path: '/galleries',
+      element:
+        <>
+          <h2> Harvard Art Museum </h2>
+          <p>
+            Look, but Don&apos;t Touch. Please select a Gellery in the navigation bar.
+          </p>
+        </>
+    },
+    { 
+      path: "*",
+      element: <h2>Page Not Found</h2>
+    }
+  ]
+}
 ]);
 
 function App() {
@@ -20,3 +43,4 @@ function App() {
 }
 
 export default App;
+
--- aa14-react-router-art-museum-main/art-museum/src/components/GalleryNavigation.jsx
+++ aa14-react-router-art-museum-main/art-museum/src/components/GalleryNavigation.jsx
@@ -1,19 +1,18 @@
 // import React from 'react';
 import { NavLink } from "react-router-dom";
+import '../components/GalleryNavigation.css';
 
 const GalleryNavigation = ({galleries}) => {
-    return (
+  return (
     <div>
-    <h1>Galleries</h1>;
-    <ul>
+      <h1>Galleries</h1>
         {galleries.map((gallery) => (
-            <NavLink key={gallery.id} to={`/gallery/${gallery.id}`} className="active">
-                {gallery.name}
-            </NavLink> 
+          <NavLink key={gallery.id} to={`/gallery/${gallery.id}`} className="active">
+            {gallery.name}
+          </NavLink> 
         ))}
-    </ul>
     </div>
-    );
+  );
 };
 
 export default GalleryNavigation;
